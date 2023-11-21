fb335e46ba18539c92aca10712efe9a29dc67897
RamboXD <rnazymxan@gmail.com>
2023-11-22 00:15:12 +0600

    asd

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,11 @@
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
 import path from "path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ mode }) => {
-  const envFile = mode === "development" ? ".env" : ".env.production";
-  dotenv.config({ path: envFile });
+export default defineConfig(() => {
   return {
     plugins: [react(), tsconfigPaths()],
-    build: {
-      outDir: "build",
-      assetsDir: "assets",
-      emptyOutDir: true,
-    },
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
